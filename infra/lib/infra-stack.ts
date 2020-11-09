e169cedd9f32c2c46310bc25b493e906f6e225f6
Jon Chen <bsd@voltaire.sh>
2020-11-09 23:05:42 +0000

    cdk

---
+++ infra/lib/infra-stack.ts
@@ -0,0 +1,24 @@
+import * as cdk from '@aws-cdk/core';
+import assets = require("@aws-cdk/aws-s3-assets");
+import lambda = require("@aws-cdk/aws-lambda");
+import path = require("path");
+
+export class InfraStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const giveCatTreatsAsset = new assets.Asset(this, "giveCatTreatsZip", {
+      path: path.join(__dirname, "../../build/give-cat-treats"),
+    })
+
+    const giveCatTreatsLambda = new lambda.Function(this, "giveCatTreatsLambda", {
+      code: lambda.Code.fromBucket(
+        giveCatTreatsAsset.bucket,
+        giveCatTreatsAsset.s3ObjectKey,
+      ),
+      runtime: lambda.Runtime.GO_1_X,
+      handler: "main",
+      timeout: cdk.Duration.seconds(45),
+    })
+  }
+}
