8420ddfafa63881e89600a0fb3df44b9f5e28d1b
Jon Chen <bsd@voltaire.sh>
2023-03-07 07:39:09 +0000

    fix syntax error

--- infra/lambda/keepwarm.js
+++ infra/lambda/keepwarm.js
@@ -1,6 +1,6 @@
 const https = require('https')
 
-exports.handler = (_, _, callback) => {
+exports.handler = (event, context, callback) => {
 	let apiDomain = process.env.API_DOMAIN
 	const u = new URL('/keepwarm', apiDomain)
 	https.get(u, (res) => {
