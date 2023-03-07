deab0d53a8de777300ad067b3b502ceac8d3751c
Jon Chen <bsd@voltaire.sh>
2023-03-07 07:50:53 +0000

    fetching api domain

--- infra/lambda/keepwarm.js
+++ infra/lambda/keepwarm.js
@@ -1,9 +1,8 @@
-const https = require('https')
+let https = require('https')
 
 exports.handler = (event, context, callback) => {
-	let apiDomain = process.env.API_DOMAIN
-	const u = new URL('/keepwarm', apiDomain)
-	https.get(u, (res) => {
+	let apiDomain = 'https://' + process.env.API_DOMAIN + '/keepwarm'
+	https.get(apiDomain, (res) => {
 		callback(null, res.statusCode)
 	}).on('error', (e) => {
 		callback(Error(e))
