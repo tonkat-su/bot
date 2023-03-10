87f825e211cc253c4eb4fec8e07b29261440adaf
Jon Chen <bsd@voltaire.sh>
2023-03-10 05:54:19 +0000

    remove file upload

--- online/status_embed.go
+++ online/status_embed.go
@@ -1,18 +1,15 @@
 package online
 
 import (
-	"bytes"
 	"context"
 	"fmt"
 	"log"
-	"mime"
 	"strings"
 
 	mcpinger "github.com/Raqbit/mc-pinger"
 	"github.com/bwmarrin/discordgo"
 	"github.com/tonkat-su/bot/emoji"
 	"github.com/tonkat-su/bot/mclookup"
-	"github.com/vincent-petithory/dataurl"
 )
 
 type PrepareStatusRequest struct {
@@ -103,40 +100,7 @@ func PrepareStatus(params *PrepareStatusRequest) (*PrepareStatusResponse, error)
 
 	embed.Description = pong.Description.Text
 
-	files := []*discordgo.File{}
-	if pong.Favicon != "" {
-		favIcon, err := dataurl.DecodeString(pong.Favicon)
-		if err != nil {
-			return nil, fmt.Errorf("error decoding favicon for server '%s': %s", serverUrl, err.Error())
-		}
-
-		file := &discordgo.File{
-			Name:        "favicon",
-			ContentType: favIcon.ContentType(),
-			Reader:      bytes.NewReader(favIcon.Data),
-		}
-		attachmentName := getAttachmentName("favicon", favIcon.ContentType())
-
-		files = append(files, file)
-
-		embed.Image = &discordgo.MessageEmbedImage{
-			URL: attachmentName,
-		}
-	}
 	return &PrepareStatusResponse{
 		MessageEmbeds: []*discordgo.MessageEmbed{embed},
-		Files:         files,
 	}, nil
-}
-
-// eat any errors and assume it is .png
-func getAttachmentName(filename, contentType string) string {
-	var extension string
-	extensions, err := mime.ExtensionsByType(contentType)
-	if err != nil || len(extensions) == 0 {
-		extension = ".png"
-	} else {
-		extension = extensions[0]
-	}
-	return strings.Join([]string{"attachment://", filename, extension}, "")
 }
